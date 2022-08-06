a5fab6b628bb2dfc00162b91701e62a48ba8c29a
Rob Mosher <rob@robmosher.com>
2022-08-06 08:23:23 -0400

    Sync changes made directly on server

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+  <head>
+    <title>Who is the Cutest?</title>
+    <meta name="viewport" content="width=660, initial-scale=1.0" />
+    <style type="text/css">
+      * {
+        margin: 0;
+        padding: 0;
+      }
+      body {
+        background-color: #570a8d;
+        text-align: center;
+      }
+      #content-wrapper {
+        position: relative;
+        top: 48%;
+        transform: translateY(-52%);
+      }
+      p {
+        font-family: "Trebuchet MS", verdana, serif;
+        color: #7934aa;
+        padding-top: 8px;
+        font-size: 13pt;
+        text-transform: uppercase;
+      }
+      a,
+      a:link,
+      a:visited,
+      a:active {
+        color: #7934aa;
+      }
+      a:hover,
+      a:focus {
+        color: #b756fc;
+      }
+      .u {
+        font-weight: bold;
+        padding-right: 10px;
+      }
+      .m:after {
+        content: " ";
+        font-size: 80%;
+        padding: 0 5px;
+        letter-spacing: -1px;
+      }
+    </style>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(["_setAccount", "UA-6391661-4"]);
+      _gaq.push(["_trackPageview"]);
+      (function () {
+        var ga = document.createElement("script");
+        ga.type = "text/javascript";
+        ga.async = true;
+        ga.src =
+          ("https:" == document.location.protocol
+            ? "https://ssl"
+            : "http://www") + ".google-analytics.com/ga.js";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+  </head>
+  <body>
+    <div id="content-wrapper">
+      <iframe
+        style="border: 12px #400046 solid"
+        width="640"
+        height="360"
+        src="https://www.youtube.com/embed/OOscn6lrabs?rel=0&showinfo=0&controls=0"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+      <p>
+        <span class="u">YOU ARE!</span>
+        <span class="m"
+          >Music &amp; Concept:
+          <a href="https://robmosher.com/">Rob Mosher</a></span
+        ><span
+          >Video: <a href="http://7xmotion.com/">Christopher Mennuto</a></span
+        >
+      </p>
+    </div>
+  </body>
+</html>
