63c31a0979ecb68cb3e87a1efd1f05b9936d3deb
Rob Mosher <rob@robmosher.com>
2024-07-30 16:23:31 -0400

    Add CICD pipeline to AWS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-customPurple'>
       <div className='relative bottom-[10vh] rounded-2xl bg-white p-8 shadow-lg'>
-        <h1 className='text-center text-8xl font-bold uppercase'>You Are!</h1>
+        <h1 className='text-center text-8xl font-bold uppercase'>You Are!!</h1>
       </div>
     </div>
   )
