919bb4021174af409bea612565d91f5346bb2a2b
Rob Mosher <rob@robmosher.com>
2024-09-07 07:31:35 -0400

    Temp add hardcoded GA, pending dynamic load fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import loadGoogleAnalytics from './utils/analytics.ts'
+// import loadGoogleAnalytics from './utils/analytics.ts'
 import './index.css'
 
-loadGoogleAnalytics()
+// loadGoogleAnalytics()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
